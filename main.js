c0089fe33af318ced906a1ddf0afa3d835e4bd2e
Thinaya <81453008+Thinaya@users.noreply.github.com>
2022-01-23 11:50:57 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 song ="";
-leftWirstX = 0;
+leftWristX = 0;
 leftWristY = 0;
 rightWrist = 0;
 rightWrist = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
 
 function setup() {
     canvas=createCanvas(600,500);
@@ -11,13 +13,15 @@ function setup() {
     video = createCapture(VIDEO);
     video.hide();
 
-    postNet = ml5.postNet(video, modelLoaded);
-    postNet.on('pose', gotPoses);
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function gotPoses(results){
     if(results.length > 0)
     {
+        scoreLeftWrist=results[0].pose.keypoints[9].score;
+        scoreRightWrist=results[0].pose.keypoints[10].score;
         console.log(results);
         leftWristX = results[0].pose.leftWrist.x;
         leftWristY = results[0].pose.leftWrist.y;
@@ -36,14 +40,51 @@ console.log('PoseNet Is Intialized');
 
 function draw() {
     image(video, 0, 0, 600, 500);
+
+    fill("lightseagreen");
+    stroke("lightseagreen");
+    ABC_status = ABC_song.isPlaying();
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+        Believer.stop();
+        if(ABC_status == false)
+        {
+           ABC_song.play();
+           document.getElementById(song).innerHTML= "ABC_song";
+               
+        }
+    }
+
+    Believer_status = Believer.isPlaying();
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+        ABC_song.stop();
+        if(Believer_status == false)
+        {
+            Believer.play();
+            document.getElementById(song).innerHTML= "Believer";
+    
+        }
+    }
+    
+    
+
+
 }
 
 function preload() {
-    song = loadSound("music.mp3");
+    Believer = loadSound("Believer.mp3");
+    ABC_song = loadSound("ABC song.mp3");
 }
 
 function play() {
     song.setVolume(1);
     song.rate(2)
     song.play();
+}
+
+function isPlaying() {
+    song
 }
